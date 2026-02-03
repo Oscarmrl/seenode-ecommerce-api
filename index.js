480ca3ca83c147faaa487f1d2b0065ebc03178a0
Oscar-Murillo <oscaralexandermurillo500@gmail.com>
2026-02-02 18:32:30 -0600

    postgres añadido

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
+import "dotenv/config";
 import express from "express";
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const prisma = new PrismaClient({ adapter });
 
 const app = express();
+app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/products", (req, res) => {
-  res.json([
-    {
-      id: "1",
-      name: "Mause",
-      price: 2000,
-      descripcion: "Mause Gamer",
-      imagenUrl:
-        "https://images.unsplash.com/photo-1628832307345-7404b47f1751?q=80&w=1483&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-  ]);
+app.get("/products", async (req, res) => {
+  try {
+    const products = await prisma.product.findMany();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 app.listen(PORT, () => {
   console.log(`Server corriendo en el puerto ${PORT}`);
+});
+
+process.on("SIGINT", async () => {
+  await prisma.$disconnect();
+  process.exit(0);
 });
